64f2c3e27b658df2b15ff210e9cd384de1014e44
Meghna Das <55181652+Meghna-DAS@users.noreply.github.com>
2020-09-20 11:58:26 +0530

    Kriti Problem Statement

---
+++ ClassWork Questions/Problem Statements/PrefixSearch.java
@@ -0,0 +1,24 @@
+ class PrefixSearch
+{
+public static void main(String[] args)
+{
+String[] arr ={"Meghna","Das","this","side"};
+String str="MeghnaDas";
+
+boolean result=false;
+
+for (int i=0;i<3;i++)
+{
+	if(str.startsWith(arr[i]))
+	{
+		result=true;
+		break;
+	}
+}
+if (result)
+	System.out.println("Given String" + " starts with one of the prefixes.");
+else
+	System.out.println("Given String do not" + " starts with one of the prefixes.");
+}
+}
+
